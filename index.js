96c5936498530f534f3481f3c5edcfc1a09e2fc5
Carlos Gonzalez <carlos.gonzalez@anima.edu.uy>
2024-04-03 08:41:02 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const handleclick = () => {
+    console.log("click");
+};
+var num1 = 3;
+var num2 = 3;
+function suma(num1, num2) {
+    var sumado = parseInt(num1) + parseInt(num2);
+    console.log(sumado);
+}
+
+function sumainput() {
+    var resultado = document.getElementById("resultado");
+    var numero1 = parseFloat(document.getElementById("input1").value);
+    var numero2 = parseFloat(document.getElementById("input2").value);
+    var sumado = numero1 + numero2;
+    resultado.innerText = "resultado:" + sumado;
+}
